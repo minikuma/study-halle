0722d698187d4ceb0641043fe7445838ba07c4e5
minikuma <minikuma@naver.com>
2021-01-08 13:24:27 +0900

    week8.ver.1.0

---
+++ src/main/java/me/minikuma/week8/HankookTire.java
@@ -0,0 +1,7 @@
+package me.minikuma.week8;/**
+ * Created by wminikuma@gmail.com on 2021/01/06
+ * Blog : https://minikuma-laboratory.tistory.com/
+ * Github : http://github.com/minikuma
+ */
+public class HankookTire {
+}
